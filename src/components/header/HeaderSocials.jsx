3b65078f10728202df6b99d380040982d78c4f61
nikhiljaiswal007 <shortsnj@gmail.com>
2022-06-11 13:13:26 +0530

    add social links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,9 +7,9 @@ import "./header.css";
 function HeaderSocials() {
   return (
    <div className="header_socials">
-      <a href='https://linkedin.com' target="_blank"><BsLinkedin/></a>
-      <a href='https://github.com' target="_blank"><FaGithub/></a>
-      <a href='https://instagram.com' target="_blank"><FaInstagram/></a>
+      <a href='https://www.linkedin.com/in/nikhil-jaiswal-1b8872209' target="_blank"><BsLinkedin/></a>
+      <a href='https://github.com/nikhiljaiswal007' target="_blank"><FaGithub/></a>
+      <a href='https://instagram.com/nnikkkhil' target="_blank"><FaInstagram/></a>
    </div>
   )
 }
